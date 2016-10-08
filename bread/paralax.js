5520c6b424917a0d5643550707b242ee0ffcd77c
pursentiy <pursentiy@gmail.com>
2016-10-08 16:31:54 +0300

    а

--- bread/paralax.js
+++ bread/paralax.js
@@ -1,9 +1,26 @@
 $(document).ready(function(){
 
-var st= $(this).scrollTop();
 
-console.log(st);
+$('div[href^="#"]').click(function(){
+var target = $(this).attr('href');
+$('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+return false; 
+});
 
 
+	var touch = $('#touch-menu');
+	var menu = $('.back');
 
+	$(touch).on('click',function(e){
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+    $(window).resize(function(){
+    	var wid = $(window).width();
+    	if(wid > 990 && menu.is(':hidden')){
+    		menu.removeAttr('style');
+    	}
+    });
+	
 });
