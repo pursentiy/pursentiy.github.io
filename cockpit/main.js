359cdd26445cfe1cb7e691b0f12c7f457533a4f1
pursentiy <pursentiy@gmail.com>
2017-02-02 19:33:17 +0300

    авы

--- cockpit/main.js
+++ cockpit/main.js
@@ -1,4 +1,82 @@
 $(function() {
+
+//Меню
+$('.button_toggle').on('click',function(){
+				$('.main_navigation').toggleClass('open');
+			});
+
+//Вкладка1
+$('#first').on('click',function(){
+	console.log("Pressed")
+				$('header .mainBlock1').css({
+					display: "block"
+				});
+				$('header .mainBlock2').css({
+					display: "none"
+				});
+				$('header .mainBlock3').css({
+					display: "none"
+				});
+				$('header .mainBlock4').css({
+					display: "none"
+				});
+			});
+
+
+//Вкладка 2
+$('#second').on('click',function(){
+	console.log("Pressed")
+				$('header .mainBlock1').css({
+					display: "none"
+				});
+				$('header .mainBlock2').css({
+					display: "block"
+				});
+				$('header .mainBlock3').css({
+					display: "none"
+				});
+				$('header .mainBlock4').css({
+					display: "none"
+				});
+			});
+
+//Вкладка 3
+$('#third').on('click',function(){
+	console.log("Pressed")
+				$('header .mainBlock1').css({
+					display: "none"
+				});
+				$('header .mainBlock2').css({
+					display: "none"
+				});
+				$('header .mainBlock3').css({
+					display: "block"
+				});
+				$('header .mainBlock4').css({
+					display: "none"
+				});
+			});
+
+
+//Вкладка 4
+$('#fourth').on('click',function(){
+	console.log("Pressed")
+				$('header .mainBlock1').css({
+					display: "none"
+				});
+				$('header .mainBlock2').css({
+					display: "none"
+				});
+				$('header .mainBlock3').css({
+					display: "none"
+				});
+				$('header .mainBlock4').css({
+					display: "block"
+				});
+			});
+
+
+
 	var minH = -95, maxH = 95, click = 0, minV=-15, maxV=34, minHow=0, maxHow=361;
 
 var timerId;
@@ -8,16 +86,16 @@ var timerId;
 /* Вертикальное отклонение*/
 		if(click%2!=0){
 			timerIdRoll = setInterval(function() {
-				$('header .mainBlock .cockpit .elements .blockHaw .cirle').css({
+				$('header .mainBlock1 .cockpit .elements .blockHaw .cirle').css({
 					transform: 'rotate('+(minH + Math.random() * (maxH - minH)) +'deg)'
 				});
-			    $('header .mainBlock .cockpit .elements .blockHaw .background').css({
+			    $('header .mainBlock1 .cockpit .elements .blockHaw .background').css({
 					top: (minV + Math.random() * (maxV - minV)) +'px'
 				});
 			},1400);
 
 			timerIdHow = setInterval(function() {
-             $('header .mainBlock .cockpit .elements .blockRoll .background').css({
+             $('header .mainBlock1 .cockpit .elements .blockRoll .background').css({
 					transform: 'rotate('+(minHow + Math.random() * (maxHow - minHow)) +'deg)'
 				});
 
@@ -34,17 +112,20 @@ var timerId;
 			
 			clearInterval(timerIdRoll);
 		    clearInterval(timerIdHow);
-			$('header .mainBlock .cockpit .elements .blockHaw .cirle').css({
+			$('header .mainBlock1 .cockpit .elements .blockHaw .cirle').css({
 					transform: 'rotate(0deg)'
 				});
-			$('header .mainBlock .cockpit .elements .blockHaw .background').css({
+			$('header .mainBlock1 .cockpit .elements .blockHaw .background').css({
 					top: '8px'
 				});
-             $('header .mainBlock .cockpit .elements .blockRoll .background').css({
+             $('header .mainBlock1 .cockpit .elements .blockRoll .background').css({
 					transform: 'rotate(0deg)'
 				});
 
 		}
 	});
+
+
+
 
 });
