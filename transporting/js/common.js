aa144b1e5f6366d7d581995e123e36d8ba55d1e2
pursentiy <pursentiy@gmail.com>
2017-08-29 22:23:02 +0300

    а

--- transporting/js/common.js
+++ transporting/js/common.js
@@ -5,5 +5,31 @@ $('.button_toggle').on('click',function(){
   $('.main_navigation').toggleClass('scroll');
   $('.wrapper').toggleClass('wrapper_small');
 });
+
+  $("#owl-demo-review").owlCarousel({
+    loop:true,
+    items:1,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    singleItem:true
+  });
+
+    $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').css("display:block")
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+
+    $('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
 });
 
