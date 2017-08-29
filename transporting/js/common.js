1ce7c83f2768e7c1ae4eb64e48585603c087f07a
pursentiy <pursentiy@gmail.com>
2017-08-29 14:11:34 +0300

    df

---
+++ transporting/js/common.js
@@ -0,0 +1,9 @@
+$(function() {
+
+//$('body').fadeOut()
+$('.button_toggle').on('click',function(){
+  $('.main_navigation').toggleClass('scroll');
+  $('.wrapper').toggleClass('wrapper_small');
+});
+});
+
