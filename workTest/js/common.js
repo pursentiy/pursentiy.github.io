dbc1abd5e7c75d8a67a90ee03880f98e12e4cac7
pursentiy <pursentiy@gmail.com>
2019-01-11 15:58:01 +0300

    ке

--- workTest/js/common.js
+++ workTest/js/common.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
 
+    var disableFullpageRes = 1025;
+    var updateCssStyles = 768;
+
     $('.button_toggle').on('click', function () {
         $('.main_navigation').toggleClass('scroll');
         $('.wrapper').toggleClass('wrapper_small');
@@ -30,33 +33,42 @@ $(document).ready(function () {
         singleItem: true
     });
 
-    $('#fullpage').fullpage({
-        'verticalCentered': false,
-        'css3': true,
-        'sectionsColor': ['#F0F2F4', '#fff', '#fff', '#fff'],
-        'navigation': true,
-        'navigationPosition': 'right',
-        'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
-
-        'afterLoad': function (anchorLink, index) {
-            if (index == 2) {
-                $('#iphone3, #iphone2, #iphone4').addClass('active');
-            }
-        },
-
-        'onLeave': function (index, nextIndex, direction) {
-            if (index == 3 && direction == 'down') {
-                $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
-            }
-            else if (index == 3 && direction == 'up') {
-                $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
-            }
-
-            $('#staticImg').toggleClass('active', (index == 2 && direction == 'down') || (index == 4 && direction == 'up'));
-            $('#staticImg').toggleClass('moveDown', nextIndex == 4);
-            $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
+$(document).ready(function() {
+    if($(window).width() < disableFullpageRes) { 
+        // do any 480 width stuff here, or simply do nothing
+        return;
+        } else {
+            $('#fullpage').fullpage({
+                'verticalCentered': false,
+                'css3': true,
+                'sectionsColor': ['#F0F2F4', '#fff', '#fff', '#fff'],
+                'navigation': true,
+                'navigationPosition': 'right',
+                'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
+        
+                'afterLoad': function (anchorLink, index) {
+                    if (index == 2) {
+                        $('#iphone3, #iphone2, #iphone4').addClass('active');
+                    }
+                },
+        
+                'onLeave': function (index, nextIndex, direction) {
+                    if (index == 3 && direction == 'down') {
+                        $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
+                    }
+                    else if (index == 3 && direction == 'up') {
+                        $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
+                    }
+        
+                    $('#staticImg').toggleClass('active', (index == 2 && direction == 'down') || (index == 4 && direction == 'up'));
+                    $('#staticImg').toggleClass('moveDown', nextIndex == 4);
+                    $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
+                }
+            });
         }
-    });
+        });
+
+ 
 
     const settingCss = (e) => {
         //первая прокрутка
@@ -66,12 +78,19 @@ $(document).ready(function () {
         let owlCarouselRect = document.getElementById("owlCarouselRect");
         let sizeOwlCarouselRect = owlCarouselRect.getBoundingClientRect();
         //let cssProp = $(window).width() - owlCarouselRect.clientWidth - sizeOwlCarouselRect.right;
-        console.log(fpNav.clientHeight);
-        if ($(window).width() > 763) {
+      //  console.log(fpNav.clientHeight);
+        if ($(window).width() > updateCssStyles) {
+            if($(window).width() > disableFullpageRes){
             $(fpNav).css({ "top": sizeOwlCarouselRect.bottom - fpNav.clientHeight + $(".owl-dots").height() + "px", "width": sizeOwlCarouselRect.left + "px" });
+            $(owlDots[0]).css({ "margin-top": -fpNav.clientHeight + "px" });
+            }
+
             $(".social").css({ "left": -sizeOwlCarouselRect.left + "px", "width": sizeOwlCarouselRect.left + "px" });
             $(".logo").css({ "left": (sizeOwlCarouselRect.left - 137) / 2 + "px" });
-            $(owlDots[0]).css({ "margin-top": -fpNav.clientHeight + "px" });
+
+            $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - $(".pannel").width() + $(".pannel .leftSide").width() + "px" });
+        }
+        else{
             $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - $(".pannel").width() + $(".pannel .leftSide").width() + "px" });
         }
     }
