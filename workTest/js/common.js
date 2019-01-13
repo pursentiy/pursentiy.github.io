2ed0034ed296e537c36456807671c60538c5bf48
pursentiy <pursentiy@gmail.com>
2019-01-14 01:24:33 +0300

    dasd

--- workTest/js/common.js
+++ workTest/js/common.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
-    var disableFullpageRes = 1025;
-    var updateCssStyles = 768;
+    const disableFullpageRes = 1025;
+    const updateCssStyles = 768;
+    const pannelWidth = 400;
 
     $('.button_toggle').on('click', function () {
         $('.main_navigation').toggleClass('scroll');
@@ -33,10 +34,10 @@ $(document).ready(function () {
         singleItem: true
     });
 
-$(document).ready(function() {
-    if($(window).width() < disableFullpageRes) { 
-        // do any 480 width stuff here, or simply do nothing
-        return;
+    $(document).ready(function () {
+        if ($(window).width() < disableFullpageRes) {
+            // do any 480 width stuff here, or simply do nothing
+            return;
         } else {
             $('#fullpage').fullpage({
                 'verticalCentered': false,
@@ -45,13 +46,13 @@ $(document).ready(function() {
                 'navigation': true,
                 'navigationPosition': 'right',
                 'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
-        
+
                 'afterLoad': function (anchorLink, index) {
                     if (index == 2) {
                         $('#iphone3, #iphone2, #iphone4').addClass('active');
                     }
                 },
-        
+
                 'onLeave': function (index, nextIndex, direction) {
                     if (index == 3 && direction == 'down') {
                         $('.section').eq(index - 1).removeClass('moveDown').addClass('moveUp');
@@ -59,62 +60,98 @@ $(document).ready(function() {
                     else if (index == 3 && direction == 'up') {
                         $('.section').eq(index - 1).removeClass('moveUp').addClass('moveDown');
                     }
-        
+
                     $('#staticImg').toggleClass('active', (index == 2 && direction == 'down') || (index == 4 && direction == 'up'));
                     $('#staticImg').toggleClass('moveDown', nextIndex == 4);
                     $('#staticImg').toggleClass('moveUp', index == 4 && direction == 'up');
                 }
             });
         }
-        });
 
- 
+        let offsetWidth;
 
-    const settingCss = (e) => {
-        //первая прокрутка
         let fpNav = document.getElementById("fp-nav");
         let owlDots = document.getElementsByClassName("owl-dots");
 
+        let header = document.getElementById("headerMenu");
+        let sizeHeader = header.getBoundingClientRect();
+
+        let owlStageOuter = document.getElementsByClassName("owl-stage-outer")[0];
+        let sizeOwlStageOuter = owlStageOuter.getBoundingClientRect();
+
+
         let owlCarouselRect = document.getElementById("owlCarouselRect");
         let sizeOwlCarouselRect = owlCarouselRect.getBoundingClientRect();
-        //let cssProp = $(window).width() - owlCarouselRect.clientWidth - sizeOwlCarouselRect.right;
-      //  console.log(fpNav.clientHeight);
-        if ($(window).width() > updateCssStyles) {
-            if($(window).width() > disableFullpageRes){
-            $(fpNav).css({ "top": sizeOwlCarouselRect.bottom - fpNav.clientHeight + $(".owl-dots").height() + "px", "width": sizeOwlCarouselRect.left + "px" });
-            $(owlDots[0]).css({ "margin-top": -fpNav.clientHeight + "px" });
+        const settingCss = (e) => {
+            //первая прокрутка
+
+            //let cssProp = $(window).width() - owlCarouselRect.clientWidth - sizeOwlCarouselRect.right;
+            //  console.log(fpNav.clientHeight);
+            if ($(window).width() > updateCssStyles) {
+                if ($(window).width() > disableFullpageRes) {
+                    $(fpNav).css({ "top": sizeOwlCarouselRect.bottom - fpNav.clientHeight + $(".owl-dots").height() + "px", "width": sizeOwlStageOuter.left + "px" });
+                    $(owlDots[0]).css({ "margin-top": -fpNav.clientHeight + "px" });
+                }
+                $(".social").css({ "left": -sizeOwlCarouselRect.left + "px", "width": sizeOwlStageOuter.left  + "px" });
+                $(".logo").css({ "left": (sizeOwlStageOuter.left - 137) / 2 + "px" });
+                $("#headerMenu").css({ "margin-left": sizeOwlStageOuter.left + "px", "width": $(owlStageOuter).innerWidth() + "px" });
+                //header.style.marginLeft= `${$("#pannel").width() - sizeOwlStageOuter.right}px`;
+                console.log(`Оступ справа: ${sizeOwlCarouselRect.left - sizeOwlStageOuter.left}`);
+                console.log(`Оступ слева: ${sizeOwlStageOuter.right - $(owlStageOuter).innerWidth()}`);
+
+                $(".pannel").css({ "margin-top": +fpNav.clientHeight / 4 + "px" });
+              //  $(".pannel").css({ "right": -$(window).width() + sizeOwlStageOuter.right - $(".pannel").width() + "px" });
+
+                const setPannelWidth = (width) => {
+                    $(".pannel").css({ "width": width + 'px',  "right": -$(window).width() + sizeOwlCarouselRect.right - width + 'px'});
+                    $("#mainLabel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right + $('.leftSide').width() + 90  + "px" });
+                }
+                $(".owl-stage-outer").width()
+                //setting pannel width accodring to state
+                const liHeader = $(".headerMenu li");
+                for (let i = liHeader.length - 1; i >= 0; i--) {
+                    let buf = liHeader[i].children[0];
+                    const offset =  $(buf).offset()
+                    console.log($(buf).width());
+                    offsetWidth = -offset.left + $(window).width();
+                    if (offsetWidth > pannelWidth) {
+                        setPannelWidth(offsetWidth);
+                        return;
+                    };
+                }
+
             }
-
-            $(".social").css({ "left": -sizeOwlCarouselRect.left + "px", "width": sizeOwlCarouselRect.left + "px" });
-            $(".logo").css({ "left": (sizeOwlCarouselRect.left - 137) / 2 + "px" });
-
-            $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - $(".pannel").width() + $(".pannel .leftSide").width() + "px" });
+            else {
+                $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right + "px" });
+            }
         }
-        else{
-            $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - $(".pannel").width() + $(".pannel .leftSide").width() + "px" });
-        }
-    }
 
-    let fromOnclick = false;
-    $(".pannel .leftSide").click(function () {
-        let owlCarouselRect = document.getElementById("owlCarouselRect");
-        let sizeOwlCarouselRect = owlCarouselRect.getBoundingClientRect();
-        if (!fromOnclick) {
-            $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right + "px" });
-            fromOnclick = true;
-        }
-        else {
-            $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - $(".pannel").width() + $(".pannel .leftSide").width() + "px" });
-            fromOnclick = false
-        }
-    });
+        let fromOnclick = false;
+        $(".pannel").click(function () {
+            if (!fromOnclick) {
+                $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right + "px" });
+                $(".leftSide").toggleClass("pannelAppear");
+                fromOnclick = true;
+            }
+            else {
+                $(".pannel").css({ "right": -$(window).width() + sizeOwlCarouselRect.right - offsetWidth + "px" });
+                $(".leftSide").toggleClass("pannelAppear");
+                fromOnclick = false
+            }
+        });
 
 
-    $(window).resize(function () {
-       
+        $(window).resize(function () {
+
+            settingCss();
+
+        });
+
         settingCss();
-        
+
     });
 
-    settingCss();
+
+
+
 });
